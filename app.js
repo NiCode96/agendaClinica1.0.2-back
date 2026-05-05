57410ee7ef4ce20175c7fdd87eff4174709cf57f
NativeCode <tm.nicolasmachuca@gmail.com>
2026-05-05 15:04:21 -0400

    IMAGEN CARGADAS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import productoRoute from "./view/productoRoutes.js";
 import tituloRoutes from "./view/tituloRoutes.js";
 import textosRoutes from "./view/textosRoutes.js";
@@ -37,8 +39,11 @@ import recetasPacientesRoutes from "./view/recetasPacientesRoutes.js";
 
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 
 const corsConfig = {
